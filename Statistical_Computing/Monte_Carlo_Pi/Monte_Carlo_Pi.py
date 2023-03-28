379109b902bb62b1d61e48c6737415812a70539d
Kevin Mora <sci.kevinmora@gmail.com>
2023-03-28 16:42:36 -0700

    Add files via upload

---
+++ Statistical_Computing/Monte_Carlo_Pi/Monte_Carlo_Pi.py
@@ -0,0 +1,98 @@
+from __future__ import division
+from random import random
+from math import pi
+import matplotlib.pyplot as plt
+"""
+Script to simulate rain in a square field. 
+
+Counting the number of rain drops in the inscribed circle of radius equal to the length of the field. 
+The ratio of the number of drops in the circle to the total number of drops gives $\pi$.
+"""
+
+
+def rain_drop(length_of_field=1):
+    """
+    Simulate a random rain drop.
+    """
+    return [(.5 - random()) * length_of_field, (.5 - random()) * length_of_field]
+
+
+def is_point_in_circle(point, length_of_field=1):
+    """
+    Return TRUE if point is in inscribed circle.
+    """
+    return (point[0]) ** 2 + (point[1]) ** 2 <= (length_of_field / 2) ** 2
+
+
+def plot_rain_drops(drops_in_circle, drops_out_of_circle, length_of_field=1, format='pdf'):
+    """ Function to draw rain drops. """
+    number_of_drops_in_circle = len(drops_in_circle)
+    number_of_drops_out_of_circle = len(drops_out_of_circle)
+    number_of_drops = number_of_drops_in_circle + number_of_drops_out_of_circle
+    plt.figure()
+    plt.xlim(-length_of_field / 2, length_of_field / 2)
+    plt.ylim(-length_of_field / 2, length_of_field / 2)
+    plt.scatter([e[0] for e in drops_in_circle], [e[1] for e in drops_in_circle], color='blue', label="Drops in circle")
+    plt.scatter([e[0] for e in drops_out_of_circle], [e[1] for e in drops_out_of_circle], color='black', label="Drops out of circle")
+    plt.legend(loc="center")
+    plt.title("%s drops: %s landed in circle, estimating $\pi$ as %.4f." % (number_of_drops, number_of_drops_in_circle, 4 * number_of_drops_in_circle / number_of_drops))
+    plt.savefig("%s_drops.%s" % (number_of_drops, format))
+
+
+def rain(number_of_drops=100, length_of_field=1, plot=True, format='pdf', dynamic=False):
+    """
+    Function to make rain drops.
+    """
+    number_of_drops_in_circle = 0
+    drops_in_circle = []
+    drops_out_of_circle = []
+    pi_estimate = []
+    
+    for k in range(number_of_drops):
+        d = (rain_drop(length_of_field))
+        if is_point_in_circle(d, length_of_field):
+            drops_in_circle.append(d)
+            number_of_drops_in_circle += 1
+        else:
+            drops_out_of_circle.append(d)
+        if dynamic:  # The dynamic option if set to TRUE will plot every new drop (this can be used to create animations of the simulation).
+            print ("Plotting drop number: %s" % (k + 1))
+            plot_rain_drops(drops_in_circle, drops_out_of_circle, length_of_field, format)
+        pi_estimate.append(4 * number_of_drops_in_circle / (k + 1))  # This updates the list with the newest estimate for pi.
+    
+    # Plot the pi estimates.
+    plt.figure()
+    plt.scatter(range(1, number_of_drops + 1), pi_estimate)
+    max_x = plt.xlim()[1]
+    plt.hlines(pi, 0, max_x, color='black')
+    plt.xlim(0, max_x)
+    plt.title("$\pi$ estimate against number of rain drops")
+    plt.xlabel("Number of rain drops")
+    plt.ylabel("$\pi$")
+    plt.savefig("Pi_estimate_for_%s_drops_thrown.pdf" % number_of_drops)
+
+    if plot and not dynamic:
+        # If the plot option is passed and matplotlib is installed this plots
+        # the final set of drops
+        plot_rain_drops(drops_in_circle, drops_out_of_circle, length_of_field, format)
+
+    return [number_of_drops_in_circle, number_of_drops]
+
+
+if __name__ == "__main__":
+    # Run the script from cli
+    from sys import argv
+    number_of_drops = 10000000
+    if len(argv) > 1:  # If an argument is passed then change number of drops to be simulated.
+        number_of_drops = eval(argv[1])
+        
+    # Two sets of simulations (comment out the unwanted one).
+    # r = rain(number_of_drops, plot = True, format = 'png', dynamic = True)
+    r = rain(number_of_drops, plot=True, format='png', dynamic=False)
+    
+    # Print to screen:
+    print ("----------------------")
+    print ("%s drops" % number_of_drops)
+    print ("pi estimated as:")
+    print ("\t%s" % (4 * r[0] / r[1]))
+    print ("----------------------")
---
+++ Statistical_Computing/Monte_Carlo_Pi/README.md
@@ -0,0 +1,28 @@
+# Monte Carlo Simulation – Estimating π
+
+<img alt="Lines of code" src="https://img.shields.io/tokei/lines/github/morkev/monte-carlo-pi"> ![](https://img.shields.io/appveyor/build/gruntjs/grunt) <img alt="GitHub repo file count" src="https://img.shields.io/github/directory-file-count/morkev/monte-carlo-pi">
+
+
+Simulates rain in a square field, where the number of rain drops in the inscribed circle of radius is equal to the length of the field. The ratio of the number of drops in the circle estimates the value of π.
+
+<b>Run this in your terminal:</b> `git clone https://github.com/morkev/monte-carlo-pi.git`
+
+---
+
+```python
+100 drops estimate the value to ~3.0800
+```
+
+<img width="450" alt="yikes" src="https://user-images.githubusercontent.com/83437383/190880902-32d6189f-74b7-4165-a25e-b0e2f6b5ce5b.png">
+
+```python
+1,000,000 drops estimate the value to ~3.1427
+```
+
+<img width="450" alt="yikes" src="https://user-images.githubusercontent.com/83437383/190880992-fec4a678-807a-4ec4-b2c4-be780fa70ce1.png">
+
+```python
+10,000,000 drops estimate the value to ~3.1418
+```
+
+<img width="450" alt="yikes" src="https://user-images.githubusercontent.com/83437383/190881000-76bde149-8390-4bec-84df-6134aed7661c.png">
